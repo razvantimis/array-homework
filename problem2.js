53dc572c6b48715e33298793d01d459c385ee22a
Razvan Timis <timisrazvanvasile@gmail.com>
2020-02-22 18:03:17 +0200

    Add problem2

---
+++ problem2.js
@@ -0,0 +1,7 @@
+// display in the console the odd numbers from 1 to 20
+const length = 20;
+for (let index = 1; index <= length; index++) {
+    if (index % 2 !== 0) {
+        console.log(index)
+    }
+}
